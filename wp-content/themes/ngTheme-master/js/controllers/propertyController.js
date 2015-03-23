dbf84095ddfa2c0489582134c0ae7670ad5e2155
JovanZDR <jovanzdrave@gmail.com>
2015-03-23 15:35:36 +0100

    Added ng-click so a customer can go to property/images.html and ng-click to go back to the property.html from  property/images.html

--- wp-content/themes/ngTheme-master/js/controllers/propertyController.js
+++ wp-content/themes/ngTheme-master/js/controllers/propertyController.js
@@ -1,10 +1,20 @@
-app.controller("propertyController", ["$scope", "Property", "$routeParams", "SITE_INFO", function($scope, Property, $routeParams, SITE_INFO) {
-	
+app.controller("propertyController", ["$scope", "Property", "$routeParams", "SITE_INFO", "$location", function($scope, Property, $routeParams, SITE_INFO, $location) {
+	$scope.goTo = function(url) {
+    $location.url(url);
+  };
+
 	console.log("propertyController is alive! routeParams: ", $routeParams);
 
 	Property.find($routeParams);
 	$scope.partialsDir = SITE_INFO.partials;
+  $scope.gotoImages = function(){
+  	$scope.goTo($location.$$path + "/images");
 
+  	//$scope.goTo($location.$path.replace(/\/images/g,""));
+  }
+  $scope.goBack = function() {
+  	$scope.goTo($location.$$path.replace(/\/images/g,""));
+  }
 	$scope.$on("foundProperty", function(event, data) {
 		console.log("propertyController on foundProperty: ", data);
 
@@ -14,6 +24,7 @@ app.controller("propertyController", ["$scope", "Property", "$routeParams", "SIT
 		$scope.property = data[0];
 	});
 
+	
 }]);
 app.filter('capitalize', function() {
   return function(data) {
