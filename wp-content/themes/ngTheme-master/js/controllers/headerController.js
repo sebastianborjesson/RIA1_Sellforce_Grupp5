c3d7f672a1907ba8838eaabdd303e13aaa130eb9
JSandis <johanna_sandis@hotmail.com>
2015-03-25 14:21:35 +0100

    headerController including listening to broadcast & goTo-function

--- wp-content/themes/ngTheme-master/js/controllers/headerController.js
+++ wp-content/themes/ngTheme-master/js/controllers/headerController.js
@@ -1,5 +1,26 @@
-//"ngTheme" controller.
-app.controller("headerController", ["$scope", "$location", function($scope, $location) {
+app.controller("headerController", ["$scope", "$location", "Menus", "SITE_INFO", function($scope, $location, Menus, SITE_INFO) {
   console.log("headerController is alive!");
+
+  $scope.partialsDir = SITE_INFO.partials;
+
+  Menus.get(7); //Our "Main Menu" in WP has id 7
+
+  $scope.$on("gotMenuLinks", function(event, data) {
+  	console.log("headerController on gotMenuLinks: ", data);
+  	$scope.menuLinks = data.items;
+  });
+
+  $scope.goTo = function(url, hardReload) {
+  	if(hardReload) {
+  		url = url.indexOf("/") === 0 ?
+  			SITE_INFO.http_root + url.substr(1) :
+  			SITE_INFO.http_root + url;
+
+  		window.location.href = url;
+  		return;
+  	}
+
+  	$location.url(url);
+  }
 
 }]);
