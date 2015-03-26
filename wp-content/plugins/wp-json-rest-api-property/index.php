e7a9eb1579bf862b1f40ffbab4ffae7e310be15a
Sebastian Börjesson <sebastianborjesson@Sebastians-MacBook-Pro.local>
2015-03-26 11:59:39 +0100

    added hugos new plugins

---
+++ wp-content/plugins/wp-json-rest-api-property/index.php
@@ -0,0 +1,16 @@
+<?php
+
+include_once(__DIR__ . "/plugin.php");
+
+/*
+
+  Plugin Name: WP JSON REST API Property
+  Plugin URI: TBA
+  Description: This plugin adds support for querying the custom content type "Property" using the WP JSON REST API
+  Author: Hugo Leandersson
+  Version: 1.0
+  Author URI: TBA
+
+
+  Please see plugin.php for all code...
+*/
